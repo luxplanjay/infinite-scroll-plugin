bd74bffaf9f0ce1236a81986d455302974405b8d
Alexander Repeta <axzerk@gmail.com>
2018-05-04 03:54:47 +0300

    resolves async item loading, need a promise from cb

--- src/scripts.js
+++ src/scripts.js
@@ -1,43 +1,50 @@
 class IScroll {
-  constructor({ container, action }) {
-    this.container = container;
+  constructor({ action, scrollThresholdPx = 100 }) {
+    this.container = document.documentElement;
     this.onScrollCallback = action;
+    this.scrollDiff = scrollThresholdPx;
+    this.isLoading = false;
 
     this.initScrollListener();
   }
 
   initScrollListener() {
+    this.container.style.overflowY = 'auto';
     window.addEventListener('scroll', this.handleScroll.bind(this));
   }
 
   handleScroll() {
     const { scrollHeight, scrollTop, clientHeight } = this.container;
+    const currentScrollDiff = scrollHeight - (scrollTop + clientHeight);
 
-    const scrollDiff = scrollHeight - (scrollTop + clientHeight);
-
-    if (scrollDiff < 100) {
+    if (!this.isLoading && currentScrollDiff < this.scrollDiff) {
       console.log('loading new items...');
-      this.onScrollCallback();
+      this.isLoading = true;
+      this.onScrollCallback().then(() => (this.isLoading = false));
     }
   }
 }
 
 new IScroll({
-  container: document.documentElement,
   action: addItems,
+  scrollThresholdPx: 150,
 });
 
 function addItems() {
   const list = document.querySelector('.list');
 
   let html = '';
-  let num = 50;
+  let num = 30;
 
   while (num > 0) {
     html += `<li>new item</li>`;
-
     num -= 1;
   }
 
-  list.insertAdjacentHTML('beforeend', html);
+  return new Promise(resolve => {
+    setTimeout(() => {
+      list.insertAdjacentHTML('beforeend', html);
+      resolve();
+    }, 200);
+  });
 }
